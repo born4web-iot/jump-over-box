dcc972fb11ad7917f1dba178464ac74b10d3cbdb
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-10-30 07:23:05 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,9 +2,53 @@ def on_a_pressed():
     # If this hitting bottom not added sprite can fly just by hitting button A and always wins :)
     # 
     if boyJumper.is_hitting_tile(CollisionDirection.BOTTOM):
-        boyJumper.vy = -200
+        boyJumper.vy = -250
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
+def setLevel4():
+    global gameLevel, gameUpdateTime, maxBarrelSpeed, minBarrelSpeed
+    gameLevel = 4
+    gameUpdateTime = 1000
+    maxBarrelSpeed = -120
+    minBarrelSpeed = -30
+    game.splash("Level " + str(gameLevel))
+
+def on_on_overlap(sprite, otherSprite):
+    game.game_over(False)
+sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, on_on_overlap)
+
+def setLevel3():
+    global gameLevel, gameUpdateTime, maxBarrelSpeed, minBarrelSpeed
+    gameLevel = 3
+    gameUpdateTime = 1500
+    maxBarrelSpeed = -100
+    minBarrelSpeed = -80
+    game.splash("Level " + str(gameLevel))
+def setLevel2():
+    global gameLevel, gameUpdateTime, maxBarrelSpeed, minBarrelSpeed
+    gameLevel = 2
+    gameUpdateTime = 2000
+    maxBarrelSpeed = -100
+    minBarrelSpeed = -80
+    game.splash("Level " + str(gameLevel))
+
+def on_on_score():
+    game.game_over(True)
+info.on_score(40, on_on_score)
+
+def setLevel1():
+    global gameLevel, gameUpdateTime, maxBarrelSpeed, minBarrelSpeed
+    gameLevel = 1
+    gameUpdateTime = 2000
+    maxBarrelSpeed = -80
+    minBarrelSpeed = -50
+    game.splash("Level " + str(gameLevel))
+newBarrel: Sprite = None
+BARRELS_WIN_NUMBER = 0
+minBarrelSpeed = 0
+maxBarrelSpeed = 0
+gameUpdateTime = 0
+gameLevel = 0
 boyJumper: Sprite = None
 tiles.set_current_tilemap(tilemap("""
     level1
@@ -38,3 +82,30 @@ boyJumper = sprites.create(img("""
     SpriteKind.player)
 tiles.place_on_tile(boyJumper, tiles.get_tile_location(1, 5))
 boyJumper.ay = 500
+BARRELS_PER_LEVEL = 10
+setLevel1()
+
+def on_update_interval():
+    global BARRELS_WIN_NUMBER, newBarrel
+    BARRELS_WIN_NUMBER = 10
+    newBarrel = sprites.create_projectile_from_side(img("""
+            1 e e e e e e 1 
+                    e e e e e e e e 
+                    1 1 1 1 1 1 1 1 
+                    e e e e e e e e 
+                    e e e e e e e e 
+                    1 1 1 1 1 1 1 1 
+                    e e e e e e e e 
+                    1 e e e e e e 1
+        """),
+        randint(maxBarrelSpeed, minBarrelSpeed),
+        0)
+    tiles.place_on_tile(newBarrel, tiles.get_tile_location(9, 5))
+    info.change_score_by(1)
+    if info.score() == 5:
+        setLevel2()
+    if info.score() == 10:
+        setLevel3()
+    if info.score() == 20:
+        setLevel4()
+game.on_update_interval(gameUpdateTime, on_update_interval)
--- main.ts
+++ main.ts
@@ -2,13 +2,35 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     // If this hitting bottom not added sprite can fly just by hitting button A and always wins :)
     // 
     if (boyJumper.isHittingTile(CollisionDirection.Bottom)) {
-        boyJumper.vy = -200
+        boyJumper.vy = -250
     }
+})
+info.onScore(25, function () {
+    game.gameOver(true)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     game.gameOver(false)
 })
-let projectile: Sprite = null
+function setLevel3 () {
+    gameLevel = 3
+    gameUpdateTime = 1500
+    maxBarrelSpeed = -100
+    minBarrelSpeed = -80
+    game.splash("Level " + gameLevel)
+}
+function setGameLevel (level: number, barrel_min: number, barrel_max: number, update_time: number) {
+    gameLevel = level
+    gameUpdateTime = update_time
+    maxBarrelSpeed = barrel_max
+    minBarrelSpeed = barrel_min
+    game.splash("Level " + gameLevel)
+}
+let newBarrel: Sprite = null
+let BARRELS_WIN_NUMBER = 0
+let minBarrelSpeed = 0
+let maxBarrelSpeed = 0
+let gameUpdateTime = 0
+let gameLevel = 0
 let boyJumper: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 boyJumper = sprites.create(img`
@@ -39,8 +61,11 @@ boyJumper = sprites.create(img`
     `, SpriteKind.Player)
 tiles.placeOnTile(boyJumper, tiles.getTileLocation(1, 5))
 boyJumper.ay = 500
-game.onUpdateInterval(2000, function () {
-    projectile = sprites.createProjectileFromSide(img`
+let BARRELS_PER_LEVEL = 10
+setGameLevel(1, -50, -80, 2000)
+game.onUpdateInterval(gameUpdateTime, function () {
+    BARRELS_WIN_NUMBER = 10
+    newBarrel = sprites.createProjectileFromSide(img`
         1 e e e e e e 1 
         e e e e e e e e 
         1 1 1 1 1 1 1 1 
@@ -49,7 +74,16 @@ game.onUpdateInterval(2000, function () {
         1 1 1 1 1 1 1 1 
         e e e e e e e e 
         1 e e e e e e 1 
-        `, randint(-100, -70), 0)
-    tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
+        `, randint(maxBarrelSpeed, minBarrelSpeed), 0)
+    tiles.placeOnTile(newBarrel, tiles.getTileLocation(9, 5))
     info.changeScoreBy(1)
+    if (info.score() == 5) {
+        setGameLevel(2, -80, -100, 2000)
+    }
+    if (info.score() == 10) {
+        setGameLevel(3, -80, -100, 1500)
+    }
+    if (info.score() == 15) {
+        setGameLevel(4, 1, 1, 1500)
+    }
 })
